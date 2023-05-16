a9ab536e426e1ea0efd147d2cc2bd64e8c281db3
Miguel Angel Briseño Flores <miiguelb07@gmail.com>
2023-05-15 20:19:30 -0600

    Print message added to head

--- my-first-2d-example/Assets/PrintToConsole.cs
+++ my-first-2d-example/Assets/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("This is my first printed message.");
     }
 
     // Update is called once per frame
